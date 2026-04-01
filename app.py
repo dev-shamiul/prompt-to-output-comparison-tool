8940efd1c6d090a4580fdc3d74061425262554ec
Shamiul Islam <shamiulislam2946@gmail.com>
2026-04-02 03:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -86,5 +87,7 @@ def index():
     )
 
 
+# Render compatible run configuration
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
